3195dd77f7d64e247e1ebb53544d39aa5d63d3b6
Sergio <sergio.decandelario@gmail.com>
2020-04-17 18:30:50 +0200

    Redeign the page of create your chapter

--- src/Components/Sections/CreateChapter/CreateChapter.js
+++ src/Components/Sections/CreateChapter/CreateChapter.js
@@ -1,116 +1,94 @@
-import React from "react";
-import {createMuiTheme} from '@material-ui/core/styles';
-import ExpansionPanel from '@material-ui/core/ExpansionPanel';
-import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
-import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
-import Typography from '@material-ui/core/Typography';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import React, {useState} from "react";
 import styled from "@emotion/styled";
 import TextField from "@material-ui/core/TextField";
 import Checkbox from "@material-ui/core/Checkbox";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
-import {ThemeProvider} from "emotion-theming";
-
-const defaultTheme = createMuiTheme();
-
-const customTheme = createMuiTheme({
-        heading: {
-            main: '#CCC',
-            fontSize: defaultTheme.typography.pxToRem(20),
-        },
-    },
-);
+import Divider from "@material-ui/core/Divider";
+import Alert from "@material-ui/lab/Alert";
 
 const TextFieldStyled = styled(TextField)`
     margin: 0 8px;
 `;
 
-const TypographyStyled = styled(Typography)`
-   flex-shrink: 0;
-   font-size: ${props => props.theme.heading.fontSize};
+const Section = styled.div`
+    margin-bottom: 10px;
 `;
 
 export default function CreateChapter() {
-    const [expanded, setExpanded] = React.useState('');
-    const [state, setState] = React.useState({
-        checked: false,
-    });
+    const [acceptRules, setAcceptRules] = useState(false);
+    const [chapterName, setChapterName] = useState('');
+    const [warriorName, setWarriorName] = useState('');
+    const [email, setEmail] = useState('');
+    const [formSubmitted, setFormSubmitted] = useState(false);
 
-    const handleChange = (panel) => (event, isExpanded) => {
-        setExpanded(isExpanded ? panel : false);
-    };
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setFormSubmitted(true);
+    }
 
-    const handleCheckChange = (event) => {
-        setState({...state, [event.target.name]: event.target.checked});
-    };
-
-    return <div>
-        <ExpansionPanel expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
-            <ExpansionPanelSummary
-                expandIcon={<ExpandMoreIcon/>}
-                aria-controls="panel1bh-content"
-                id="panel1bh-header"
-            >
-                <ThemeProvider theme={customTheme}>
-                    <TypographyStyled>Join the worldwide Turbojugend</TypographyStyled>
-                </ThemeProvider>
-            </ExpansionPanelSummary>
-            <ExpansionPanelDetails>
-                <Typography>
+    return <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <div>
+            <h1>Start your own Turbojugend</h1>
+            <Divider/>
+            <Section>
+                <h2>Join the worldwide Turbojugend</h2>
+                <p>
                     Check the <a href="http://turbojugend.org/map/">Turbojugend Map</a> for the chapters in your area
                     and reach out to the chapters close to you:
-                    <ul>
-                        <li>If there are no chapters in your area: Start your own.</li>
-                        <li>If there is a chapter close to you: Join them.</li>
-                        <li>If you do not want to join your local chapters: Tell us why.</li>
-                    </ul>
-                </Typography>
-            </ExpansionPanelDetails>
-        </ExpansionPanel>
-
-        <ExpansionPanel expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
-            <ExpansionPanelSummary
-                expandIcon={<ExpandMoreIcon/>}
-                aria-controls="panel2bh-content"
-                id="panel2bh-header"
-            >
-                <ThemeProvider theme={customTheme}>
-                    <TypographyStyled>Setup your chapter</TypographyStyled>
-                </ThemeProvider>
-            </ExpansionPanelSummary>
-            <ExpansionPanelDetails>
-                <Typography>
-                    <ul>
-                        <li><a href="http://www.turbojugend.org/downloads/Turbojugend-Guidelines.pdf">Check our
-                            guidelines for new chapters</a> and choose your chapter name accordingly.
-                        </li>
-                        <li>Tell us where your chapter will be located.</li>
-                    </ul>
-                </Typography>
-            </ExpansionPanelDetails>
-        </ExpansionPanel>
-
-        <ExpansionPanel expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
-            <ExpansionPanelSummary
-                expandIcon={<ExpandMoreIcon/>}
-                aria-controls="panel3bh-content"
-                id="panel3bh-header"
-            >
-                <ThemeProvider theme={customTheme}>
-                    <TypographyStyled>Become the president</TypographyStyled>
-                </ThemeProvider>
-            </ExpansionPanelSummary>
-            <ExpansionPanelDetails>
-                <form noValidate autoComplete="off">
-                    <TextFieldStyled id="warrior-name" label="Warrior name" fullWidth required/>
-                    <TextFieldStyled id="email" label="Email" fullWidth required/>
+                </p>
+                <ul>
+                    <li>If there are no chapters in your area: Start your own.</li>
+                    <li>If there is a chapter close to you: Join them.</li>
+                    <li>If you do not want to join your local chapters: Tell us why.</li>
+                </ul>
+            </Section>
+            <Divider/>
+            <Section>
+                <h2>Setup your chapter</h2>
+                <p>
+                    Check the <a href="http://turbojugend.org/map/">Turbojugend Map</a> for the chapters in your area
+                    and reach out to the chapters close to you:
+                </p>
+                <ul>
+                    <li><a href="http://www.turbojugend.org/downloads/Turbojugend-Guidelines.pdf">Check our
+                        guidelines for new chapters</a> and choose your chapter name accordingly.
+                    </li>
+                    <li>Tell us where your chapter will be located.</li>
+                </ul>
+                <TextFieldStyled
+                    label="Chapter name"
+                    fullWidth
+                    required
+                    value={chapterName}
+                    onChange={e => setChapterName(e.target.value)}
+                />
+            </Section>
+            <Divider/>
+            <Section>
+                <h2>Become the president</h2>
+                <div>
+                    <TextFieldStyled
+                        label="Warrior name"
+                        fullWidth
+                        required
+                        value={warriorName}
+                        onChange={e => setWarriorName(e.target.value)}
+                    />
+                    <TextFieldStyled
+                        label="Email"
+                        fullWidth
+                        required
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                        type="email"
+                    />
                     <FormControlLabel
                         control={
                             <Checkbox
-                                checked={state.checked}
-                                onChange={handleCheckChange}
+                                checked={acceptRules}
+                                onChange={e => setAcceptRules(e.target.checked)}
                                 name="checkedB"
                                 color="primary"
                             />
@@ -118,10 +96,22 @@ export default function CreateChapter() {
                         label="Approve our rules"
                     />
                     <Grid item xs={12}>
-                        <Button variant="contained" color="primary">Send</Button>
+                        <Button variant="contained" color="primary" type="submit">Send</Button>
                     </Grid>
-                </form>
-            </ExpansionPanelDetails>
-        </ExpansionPanel>
-    </div>
+                </div>
+            </Section>
+        </div>
+        {formSubmitted &&
+            <Alert severity="success">The information goes to the turbojugend, when all it's okey we tell you!</Alert>
+        }
+
+        {formSubmitted &&
+        <ul>
+            <li>Chapter name: {chapterName}</li>
+            <li>Warrior name: {warriorName}</li>
+            <li>Email: {email}</li>
+            <li>Accepted rules: {acceptRules ? 'YES' : 'NO'}</li>
+        </ul>
+        }
+    </form>
 };
--- src/Components/Sections/Home/Home.js
+++ src/Components/Sections/Home/Home.js
@@ -1,48 +1,9 @@
 import React from 'react';
-import NoSsr from '@material-ui/core/NoSsr';
-import {
-    createMuiTheme,
-    ThemeProvider as MuiThemeProvider,
-    darken,
-} from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import styled from "@emotion/styled";
-import {ThemeProvider} from "emotion-theming";
-
-const customTheme = createMuiTheme({
-    palette: {
-        primary: {
-            main: '#6772e5',
-        },
-    },
-});
-
-const StyledButton = styled(Button)`
-  ${({ theme }) => `
-  background-color: ${theme.palette.primary.main};
-  color: #fff;
-  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
-  padding: 4px 10px;
-  font-size: 13px;
-  &:hover {
-    background-color: ${darken(theme.palette.primary.main, 0.2)};
-  }
-  ${theme.breakpoints.up('sm')} {
-    font-size: 14px;
-    padding: 7px 14px;
-  }
-  `}
-`;
 
 export default function Home() {
     return (
-        <NoSsr>
-            <MuiThemeProvider theme={customTheme}>
-                <ThemeProvider theme={customTheme}>
-                    <Button>Default</Button>
-                    <StyledButton>Customized</StyledButton>
-                </ThemeProvider>
-            </MuiThemeProvider>
-        </NoSsr>
+        <div>
+            <h1>Turbojugend Home</h1>
+        </div>
     );
 }
