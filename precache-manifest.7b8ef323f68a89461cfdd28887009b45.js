4a4c414d2a05430eec894e154689cfde6aac51e8
Sergio <sergio.decandelario@gmail.com>
2020-04-17 19:13:02 +0200

    Updates

---
+++ precache-manifest.7b8ef323f68a89461cfdd28887009b45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd7a2f620139550d1b654c39c8f006de",
+    "url": "/Turbojugend/public-page-web/index.html"
+  },
+  {
+    "revision": "d27012338ce61b795c31",
+    "url": "/Turbojugend/public-page-web/static/css/main.f3520556.chunk.css"
+  },
+  {
+    "revision": "93ff60273a18dd3ce11c",
+    "url": "/Turbojugend/public-page-web/static/js/2.0bd67a1a.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/Turbojugend/public-page-web/static/js/2.0bd67a1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d27012338ce61b795c31",
+    "url": "/Turbojugend/public-page-web/static/js/main.d5d86881.chunk.js"
+  },
+  {
+    "revision": "15573c61ec6b364e1681",
+    "url": "/Turbojugend/public-page-web/static/js/runtime-main.c3c096be.js"
+  }
+]);
